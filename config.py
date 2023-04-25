17cc7ed39cb8bb3c0cd85d2a3a8c122601a60a3d
KSBOTSOFFICAL <108800837+KSBOTSOFFICAL@users.noreply.github.com>
2023-04-25 13:05:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 sudo_users = [959184369, 2036297424, 1640783367]
 
-iptv_link = "https://gist.githubusercontent.com/KSBOTSOFFICAL/0ac04b5da46b9b67edb0c592aa019e21/raw/72873ddfa17b265a986099ed5f9a63fd2e012921/ggs.json"
+iptv_link = "https://gist.githubusercontent.com/KSBOTSOFFICAL/0ac04b5da46b9b67edb0c592aa019e21/raw/3b5f5e9bf232ee1992c2bc790b98b40d08ec12d9/ggs.json"
 
 GROUP_TAG = "SAI"
 
