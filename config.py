b107767f0756f25797f4169a2d33556f543657e0
KSBOTSOFFICAL <108800837+KSBOTSOFFICAL@users.noreply.github.com>
2023-04-30 09:12:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-sudo_users = [959184369, 2036297424, 1640783367]
+sudo_users = [959184369, 2036297424, 1640783367, 1900583882]
 
 iptv_link = "https://gist.githubusercontent.com/KSBOTSOFFICAL/0ac04b5da46b9b67edb0c592aa019e21/raw/8f66fd4a0466900a9155beab42f16d3344a17193/Gjggbs.json"
 
